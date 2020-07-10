99c6ee5b26df4660c1c7600991e62085591ec285
jianjunyue <jianjunyue@gmail.com>
2020-07-10 18:52:35 +0800

    'code'

---
+++ temp/recommend/__init__.py
@@ -0,0 +1,16 @@
+
+
+# https://github.com/SSSxCCC/Recommender-System/tree/master/Recommender_System
+
+# Recommender-System
+# A developing recommender system, implements in tensorflow 2.
+#
+# Dataset: MovieLens-100k, MovieLens-1m, MovieLens-20m, lastfm, Book-Crossing, and some satori knowledge graph.
+#
+# Algorithm: UserCF, ItemCF, LFM, SLIM, GMF, MLP, NeuMF, FM, DeepFM, MKR, RippleNet, KGCN and so on.
+#
+# Evaluation: ctr's auc f1 and topk's precision recall.
+#
+# Requirements
+# Python 3.7
+# Tensorflow 2.1.0
